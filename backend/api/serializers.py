f3d866958e9e464541b9e75bfdbd94cc5e98ed4d
romeomanoela <romeomanoela18@gmail.com>
2025-12-04 15:56:12 +0300

    ✨ feat(auth): Make password write-only in user serializer for enhanced security

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -7,6 +7,9 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ('id', 'email', 'password', 'username', 'prenom', 'nom', 'token', 'user_id', 'baikal_password')
+        extra_kwargs = {
+            'password': {'write_only': True},
+        }
     def create(self, validated_data):
         user = User.objects.create_user(
             email=validated_data['email'],
